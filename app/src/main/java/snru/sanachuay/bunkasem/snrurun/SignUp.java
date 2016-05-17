7181f860a11de1c4eb13ba078f36c7b49b2e03ab
Ball <ball-158159@hotmail.com>
2016-05-17 00:00:41 -0700

    Update mySQL finish

--- app/src/main/java/snru/sanachuay/bunkasem/snrurun/SignUp.java
+++ app/src/main/java/snru/sanachuay/bunkasem/snrurun/SignUp.java
@@ -1,28 +1,30 @@
 package snru.sanachuay.bunkasem.snrurun;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.FormEncodingBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.RadioButton;
+        import android.widget.RadioGroup;
 
-import java.io.IOException;
+        import com.squareup.okhttp.Call;
+        import com.squareup.okhttp.Callback;
+        import com.squareup.okhttp.FormEncodingBuilder;
+        import com.squareup.okhttp.OkHttpClient;
+        import com.squareup.okhttp.Request;
+        import com.squareup.okhttp.RequestBody;
+        import com.squareup.okhttp.Response;
+
+        import java.io.IOException;
 
 public class SignUp extends AppCompatActivity {
 
     //Explicit
     private EditText nameEditText, userEditText, passwordEditText;
     private RadioGroup radioGroup;
-    private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton, choice5RadioButton;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton, choice5RadioButton;
     private String nameString, userString, passwordString, avataString = "0";
 
 
@@ -38,12 +40,15 @@ public class SignUp extends AppCompatActivity {
         radioButtonController();
 
 
-    }   //Main Method
+
+    }   // Main Method
 
     private void radioButtonController() {
+
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int i) {
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
                 switch (i) {
                     case R.id.radioButton:
                         avataString = "0";
@@ -64,7 +69,6 @@ public class SignUp extends AppCompatActivity {
 
             }
         });
-
     }
 
     private void bindWidget() {
@@ -77,43 +81,44 @@ public class SignUp extends AppCompatActivity {
         choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
         choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
         choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
-        choice5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
     }
 
     public void clickSignUpSign(View view) {
+
         //Get Value From EditText
         nameString = nameEditText.getText().toString().trim();
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
-
         //Check Space
-        if (nameString.equals("")|| userString.equals("")|| passwordString.equals("")) {
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("") ) {
+
             MyAlert myAlert = new MyAlert();
-            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องครับ");
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
 
         } else {
+
             updateUserTABLE();
 
-
-
-
         }
+
+
 
     }
 
     private void updateUserTABLE() {
+
         OkHttpClient okHttpClient = new OkHttpClient();
         RequestBody requestBody = new FormEncodingBuilder()
                 .add("isAdd", "true")
-                .add("name", nameString)
-                .add("user", userString)
-                .add("password", passwordString)
-                .add("avata", avataString)
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .add("Avata", avataString)
                 .build();
         Request.Builder builder = new Request.Builder();
-        Request request = builder.url("http://swiftcodingthai.com/snru/add_user_ball.php").post(requestBody).build();
+        Request request = builder.url("http://swiftcodingthai.com/snru/add_user_master.php").post(requestBody).build();
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
             @Override
@@ -124,9 +129,11 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onResponse(Response response) throws IOException {
                 finish();
-
             }
         });
-    }
 
-}   //Main Class
+
+
+    }   // updateUserTABLE
+
+}   // Main Class
