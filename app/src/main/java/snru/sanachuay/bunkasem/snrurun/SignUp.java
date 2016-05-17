4142ab5a6cee4552c069783b99359dea68918c50
Ball <ball-158159@hotmail.com>
2016-05-16 23:26:52 -0700

    Update mySQL finish

--- app/src/main/java/snru/sanachuay/bunkasem/snrurun/SignUp.java
+++ app/src/main/java/snru/sanachuay/bunkasem/snrurun/SignUp.java
@@ -7,13 +7,23 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUp extends AppCompatActivity {
 
     //Explicit
     private EditText nameEditText, userEditText, passwordEditText;
     private RadioGroup radioGroup;
     private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton, choice5RadioButton;
-    private String nameString, userString, passwordString, avataString;
+    private String nameString, userString, passwordString, avataString = "0";
 
 
     @Override
@@ -24,7 +34,38 @@ public class SignUp extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Radio Button Controller
+        radioButtonController();
+
+
     }   //Main Method
+
+    private void radioButtonController() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                switch (i) {
+                    case R.id.radioButton:
+                        avataString = "0";
+                        break;
+                    case R.id.radioButton2:
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avataString = "3";
+                        break;
+                    case R.id.radioButton5:
+                        avataString = "4";
+                        break;
+                }
+
+            }
+        });
+
+    }
 
     private void bindWidget() {
 
@@ -53,9 +94,39 @@ public class SignUp extends AppCompatActivity {
             myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องครับ");
 
         } else {
+            updateUserTABLE();
+
+
+
 
         }
 
+    }
+
+    private void updateUserTABLE() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("name", nameString)
+                .add("user", userString)
+                .add("password", passwordString)
+                .add("avata", avataString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/snru/add_user_ball.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+
+            }
+        });
     }
 
 }   //Main Class
