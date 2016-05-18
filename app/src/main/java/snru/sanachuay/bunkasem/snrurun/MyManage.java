59f97560dba6c31003bde2d57b9b77658c6b50f8
Ball <ball-158159@hotmail.com>
2016-05-17 23:24:47 -0700

    Design Ui Maps

--- app/src/main/java/snru/sanachuay/bunkasem/snrurun/MyManage.java
+++ app/src/main/java/snru/sanachuay/bunkasem/snrurun/MyManage.java
@@ -1,5 +1,6 @@
 package snru.sanachuay.bunkasem.snrurun;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -10,10 +11,33 @@ public class MyManage {
     //Explicit
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
+
+    public static final String user_table = "userTABLE";
+    public static final String colum_id = "_id";
+    public static final String colum_name = "Name";
+    public static final String colum_user = "User";
+    public static final String colum_password = "Password";
+    public static final String colum_avata = "Avata";
+
     public MyManage(Context context) {
         myOpenHelper = new MyOpenHelper(context);
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
 
 
     }   // Constructor
+
+    public long addUser(String strName,
+                        String strUser,
+                        String strPassword,
+                        String strAvata) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(colum_name, strName);
+        contentValues.put(colum_user, strUser);
+        contentValues.put(colum_password, strPassword);
+        contentValues.put(colum_avata, strAvata);
+
+        return sqLiteDatabase.insert(user_table, null, contentValues);
+    }
+
+
 }   //Main Class
